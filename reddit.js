bffd753fbdf953449ce9a901ec8166f4349802c7
Milo Fultz <milopfultz@gmail.com>
2021-01-02 11:56:17 -0800

    Rename elements

--- reddit.js
+++ reddit.js
@@ -1,37 +1,35 @@
 // reddit functions
 var getTopPosts = async function (subreddit, time, n=10) {
   try {
-    // Create URL
     var url = `https://www.reddit.com/r/${subreddit}/top/.json?t=${time}`;
-    // Get JSON from top posts
     return await fetch(url)
       .then(response => response.json())
-      .then(json => getOrderedTopPostData(json.data.children));
+      .then(json => extractAndOrderPostData(json.data.children));
   } catch {
     console.log(`An error occurred in getting top posts from ${subreddit} with time ${time}.`);
   }
 };
 
-var getOrderedTopPostData = function (rawPosts) {
+var extractAndOrderPostData = function (rawPosts) {
   var output = [];
-  var attributes = ["score", "title", "author", "subreddit", "url"];
+  var properties = ["score", "title", "author", "subreddit", "url"];
   for (var i = 0; i < rawPosts.length; i++) {
     var rawPost = rawPosts[i].data;
     var postData = {};
     complete = true;
-    for (var j = 0; j < attributes.length; j++) {
-      var attribute = attributes[j];
-      if (rawPost[attribute] === undefined) {
+    for (var j = 0; j < properties.length; j++) {
+      var property = properties[j];
+      if (rawPost[property] === undefined) {
         complete = false;
         break;
       }
-      postData[attribute] = rawPost[attribute];
+      postData[property] = rawPost[property];
     }
-    // Filter incomplete cases
-    if (complete) output.push(postData);
+    if (complete) {
+      output.push(postData);
+    }
   }
-
-  // Sort output data
+  // Sort high to low
   output.sort(function(a, b) {
     return parseInt(b.score) - parseInt(a.score);
   });
